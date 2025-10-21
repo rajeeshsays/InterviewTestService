2ed5a7eb142f0dfdc1d7f0d91b0f94d13d8bd6b5
rajeeshsays <rajeeshsays@gmail.com>
2025-10-21 06:47:29 +0530

    Add project files.

---
+++ InterviewTestService/Controllers/MailParseController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InterviewTestService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MailParseController : ControllerBase
+    {
+        public MailParseController() { }
+
+        [HttpPost("parsedata")]
+        public IActionResult ParsedData([FromBody] MailData data)
+        {
+            return Ok("Successfully Parsed");
+        }
+        public class MailData
+        {
+            public string data { get; set; }
+        }
+
+    }
+}
---
+++ InterviewTestService/Program.cs
@@ -0,0 +1,32 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        policy => policy
+            .WithOrigins("http://localhost:3000") // Allow this origin
+            .AllowAnyHeader()
+            .AllowAnyMethod());
+});
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors("AllowSpecificOrigin");
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
